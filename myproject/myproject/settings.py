8c705851806dcc16929046b6ff5db35c366929c0
jiyadtp <muhammedjiyadtp@gmail.com>
2024-02-27 02:03:21 +0530

    fourth commit

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -81,6 +81,18 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'book_management',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
